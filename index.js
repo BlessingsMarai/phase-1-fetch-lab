51a478f8aa37ac12da5f4d51cd0bb2eecdcb7804
Blessings <blessings.marai@student.moringaschool.com>
2023-10-03 08:30:20 +0300

    completed assingnment

--- index.js
+++ index.js
@@ -1,23 +1,10 @@
 function fetchBooks() {
-  // Make a fetch request to the Game of Thrones API
   return fetch('https://anapioficeandfire.com/api/books')
-    .then(response => {
-      // Check if the response status is OK 
-      if (!response.ok) {
-        throw new Error('Network response was not ok');
-      }
-      // Convert the response to JSON
-      return response.json();
-    })
-    .then(data => {
-      // Call the renderBooks function with the JSON data
-      renderBooks(data);
-    })
-    .catch(error => {
-      // Handle any errors that occur during the fetch
-      console.error('There was a problem with the fetch operation:', error);
-    });
+    .then(response => response.json())
+    .then(data => renderBooks(data))
+    .catch(error => console.error('Error fetching books:', error));
 }
+
 
 function renderBooks(books) {
   const main = document.querySelector('main');
